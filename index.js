fc31023a3f7a2b3c2f060d2c920058ea53f825a1
Nick Osborne <59587632+NickPCMR@users.noreply.github.com>
2022-09-01 22:29:43 -0500

    setup

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://localhost:27017/test');
+    await mongoose.connect('mongodb://localhost:27017/bjjdb')
+        .then(() => {
+            console.log("Connection open")
+        })
+        .catch(err => {
+            console.log("connection error")
+            console.log(err)
+        });
 
     // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
 }
