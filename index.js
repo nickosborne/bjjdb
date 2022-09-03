3ae05cc3017cb81262c70064e5c663824596a512
Nick Osborne <osbornic@oregonstate.edu>
2022-09-03 13:28:49 -0500

    add gitignore

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // getting-started.js
 const mongoose = require('mongoose');
+const { monitorEventLoopDelay } = require('perf_hooks');
 
 main().catch(err => console.log(err));
 
@@ -15,3 +16,21 @@ async function main() {
 
     // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
 }
+
+const positionSchema = new mongoose.Schema({
+    name: String,
+    otherNames: [String],
+    top: Boolean,
+    options: [String]
+});
+
+const Position = mongoose.model('Position', positionSchema)
+
+// Position.insertMany([
+//     { name: 'Truck', otherNames: [], top: true, options: [] },
+//     { name: 'Mount', otherNames: ['Front Mount'], top: true, options: [] },
+//     { name: 'Back', otherNames: ['Back Mount', 'Rear Mount'], top: true, options: [] },
+//     { name: 'Side Control', otherNames: [], top: true, options: ['Kesa Getame', 'Americana', 'Kimura'] },
+//     { name: 'Crucifix', otherNames: [], top: true, options: ['Short Choke', 'Arm bar'] },
+//     { name: 'Turtle', otherNames: [], top: true, options: ['Back Entry', 'Truck Entry'] },
+// ])
