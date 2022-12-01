72e4ea6b55b93ca7dcdf5ba3cd083bed1148b5b4
Nick Osborne <nrosborne@live.com>
2022-12-01 15:43:42 -0600

    basic error handling

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 const path = require('path');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 
 
 //Models
@@ -31,45 +33,54 @@ app.get('/', (req, res) => {
     res.send('home')
 })
 
-app.get('/positions', async (req, res) => {
+app.get('/positions', catchAsync(async (req, res, next) => {
     const positions = await Position.find({})
     res.render('positions/index', { positions })
-})
+}))
 
 app.get('/positions/new', (req, res) => {
     res.render('positions/new');
 });
 
-app.post('/positions', async (req, res) => {
+app.post('/positions', catchAsync(async (req, res) => {
     const position = new Position(req.body.position);
     await position.save();
     res.redirect(`/positions/${position.id}`)
-})
+}))
 
-app.get('/positions/:id', async (req, res) => {
+app.get('/positions/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const position = await Position.findById(id)
     res.render('positions/show', { position })
-})
+}))
 
-app.get('/positions/:id/edit', async (req, res) => {
+app.get('/positions/:id/edit', catchAsync(async (req, res) => {
     const { id } = req.params;
     const position = await Position.findById(id)
     res.render('positions/edit', { position })
-})
+}))
 
-app.put('/positions/:id', async (req, res) => {
+app.put('/positions/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const position = await Position.findByIdAndUpdate(id, { ...req.body.position })
     res.redirect(`/positions/${position._id}`)
-})
+}))
 
-app.delete('/positions/:id', async (req, res) => {
+app.delete('/positions/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Position.findByIdAndDelete(id);
     res.redirect('/positions');
+}))
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page not found', 404))
 })
 
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'uh oh!!!';
+    res.status(statusCode).render('error', { err });
+})
 app.listen(3000, () => {
     console.log("APP is listening on 3000")
 });
