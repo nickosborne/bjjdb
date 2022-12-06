658a3bc2e89b554200a273153e41f6ab148ab92d
Nick Osborne <nrosborne@live.com>
2022-12-06 17:04:51 -0600

    working multi select bitches!!!!

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static('public'));
 
 
 // connect to DB
