5d622a25b0023c714b917165395c2af6140005a6
Nick Osborne <nrosborne@live.com>
2023-01-15 03:24:08 -0600

    protect route

--- routes/submissions.js
+++ routes/submissions.js
@@ -14,7 +14,7 @@ router.put('/:id', forceLogin, subs.validateSubmission, catchAsync(subs.update))
 
 router.get('/:id/edit', forceLogin, catchAsync(subs.edit))
 
-router.post('/', subs.validateSubmission, catchAsync(subs.create))
+router.post('/', forceLogin, subs.validateSubmission, catchAsync(subs.create))
 
 router.post('/variations', subs.validateSubmissionVariation, catchAsync(subs.createVariation))
 
