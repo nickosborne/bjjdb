e6c063c47f9f436afdc1f4bd406ce3f08e159d99
Nick Osborne <nrosborne@live.com>
2023-10-08 16:56:18 -0500

    add Technique model

--- models/Technique.js
+++ models/Technique.js
@@ -8,19 +8,6 @@ const techniques = [
     'Escape',
     'Back Take'
 ]
-const tags = [
-    'No Gi',
-    'Choke',
-    'Wrestling',
-    'Judo',
-    'Arm Lock',
-    'Leg Lock',
-    'Pain',
-    'Collar Choke',
-    'Lapel Choke',
-    'Head and Arm Choke',
-    'Sambo'
-]
 
 const techniqueSchema = new mongoose.Schema({
     name: {
@@ -31,60 +18,19 @@ const techniqueSchema = new mongoose.Schema({
         type: [String],
         required: false
     },
-    techniqueType: {
-        type: [String],
-        enum: techniques
-    },
-    public: {
-        type: Boolean,
-        default: false
-    },
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    },
-});
-
-
-const variationSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    otherNames: {
-        type: [String],
-        required: false
-    },
-    technique: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Technique',
-        required: true
-    },
     position: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Position',
         required: true
     },
-    techniqueType: {
+    Type: {
         type: String,
         enum: techniques
-    },
-    variation: {
-        type: String,
-        enum: [
-            'Normal',
-            'Reverse',
-            'Inverted'
-        ]
     },
     side: {
         type: String,
         enum: ['Top', 'Bottom'],
         default: 'Top'
-    },
-    tags: {
-        type: String,
-        enum: tags
     },
     public: {
         type: Boolean,
@@ -96,6 +42,5 @@ const variationSchema = new mongoose.Schema({
     }
 });
 
-const Variation = mongoose.model('Variation', variationSchema);
 const Technique = mongoose.model('Technique', techniqueSchema);
-module.exports = Technique, Variation;
+module.exports = Technique;
