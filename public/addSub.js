13488ec2bb53c07ed8536bcd1a978b52f34c1e2c
Nick Osborne <nrosborne@live.com>
2024-01-23 11:49:57 -0600

    custom validation

--- public/addSub.js
+++ public/addSub.js
@@ -34,3 +34,18 @@ form.addEventListener('input', async function (e) {
         })
     }
 })
+
+const validateLink = () => {
+    const url = document.getElementById('video').value;
+    var p = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+    if (url.match(p) && url.match(p)[1]) {
+        link.classList.remove("is-invalid");
+        link.classList.add('is-valid');
+        return true;
+    }
+    else {
+        link.classList.add("is-invalid");
+        link.classList.remove('is-valid');
+        return false;
+    }
+}
