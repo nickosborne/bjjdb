bef33036ae302b56d2e558d5893757ed692cf889
Nick Osborne <nrosborne@live.com>
2024-01-23 12:20:00 -0600

    i custom validator is working!

--- public/addSub.js
+++ public/addSub.js
@@ -58,15 +58,18 @@ const validateLink = () => {
     const url = link.value
     var p = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
     if (url.match(p) && url.match(p)[1]) {
-        link.classList.remove("is-invalid");
-        link.classList.add('is-valid');
+        link.setCustomValidity("");
+        // link.classList.remove("is-invalid");
+        // link.classList.add('is-valid');
         console.log("Valid")
         return true;
     }
     else {
         console.log("invalid")
-        link.classList.add("is-invalid");
-        link.classList.remove('is-valid');
+        // link.classList.add("is-invalid");
+        // link.classList.remove('is-valid');
+        link.setCustomValidity("Invalid field.");
+
         return false;
     }
 }
